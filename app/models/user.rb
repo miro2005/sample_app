2246746a3d9a9bb7acb9fda721e4705feb27ab11
Michael Robertson <mirob2005@gmail.com>
2012-04-16 21:12:47 -0700

    Ch11 done through 11.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@
 #
 require 'digest'
 
-class User < ActiveRecord::Base 
+class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+
+  has_many :microposts, :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,13 +3,27 @@ require 'spec_helper'
 describe Micropost do
 
   before(:each) do
-    @attr = {
-      :content => "value for content",
-      :user_id => 1
-    }
+    @user = Factory(:user)
+    @attr = { :content => "value for content" }
   end
 
   it "should create a new instance given valid attributes" do
-    Micropost.create!(@attr)
+    @user.microposts.create!(@attr)
+  end
+
+  describe "user associations" do
+
+    before(:each) do
+      @micropost = @user.microposts.create(@attr)
+    end
+
+    it "should have a user attribute" do
+      @micropost.should respond_to(:user)
+    end
+
+    it "should have the right associated user" do
+      @micropost.user_id.should == @user.id
+      @micropost.user.should == @user
+    end
   end
 end
